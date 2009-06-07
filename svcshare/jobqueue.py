74a182f1c35bc0e50bedc7c4cd1ce20a176c51cb
Matt Sparks <ms@quadpoint.org>
2009-06-07 01:08:21 -0500

    add ability to remove jobs

--- svcshare/jobqueue.py
+++ svcshare/jobqueue.py
@@ -3,7 +3,7 @@ import time
 
 class Job(object):
   """Class representing a job in a JobQueue."""
-  def __init__(self, func, args=None, delay=0, periodic=False):
+  def __init__(self, func, args=None, delay=0, periodic=False, name=None):
     if args is None:
       args = []
     self.func = func
@@ -11,13 +11,17 @@ class Job(object):
     self.epoch = time.time() + delay
     self.periodic = periodic
     self.delay = delay
+    self.name = name
+
+  def __str__(self):
+    return "<Job '%s': %s>" % (self.name, self.func)
 
 
 class JobQueue(object):
   def __init__(self):
     self._queue = []
 
-  def add_job(self, func, args=None, delay=0, periodic=False):
+  def add_job(self, func, args=None, delay=0, periodic=False, name=None):
     """Add a new job to the job queue.
 
     Args:
@@ -25,8 +29,9 @@ class JobQueue(object):
       args: list of arguments to pass to the function
       delay: deadline from now in seconds
       periodic: automatically re-enqueue this job when it runs
+      name: name to assign to this job
     """
-    new_job = Job(func, delay=delay, args=args, periodic=periodic)
+    new_job = Job(func, delay=delay, args=args, periodic=periodic, name=name)
     for idx, job in enumerate(self._queue):
       if job.epoch > new_job.epoch:
         self._queue.insert(idx, new_job)
@@ -59,6 +64,21 @@ class JobQueue(object):
                    periodic=job.periodic)
     return job
 
+  def remove_job(self, name):
+    """Remove a job specified by name.
+
+    Args:
+      name: name of job to remove. A job must have a name to be removed.
+
+    Returns:
+      True if successful, False if not
+    """
+    for idx, job in enumerate(self._queue):
+      if job.name == name:
+        del self._queue[idx]
+        return True
+    return False
+
 
 if __name__ == "__main__":
   def foo():
@@ -70,10 +90,21 @@ if __name__ == "__main__":
   q = JobQueue()
   q.add_job(foo, delay=3)
   q.add_job(bar, delay=8)
+
   assert(q.next_job() is None)
-  time.sleep(3)
+  time.sleep(3.1)
   assert(q.next_job().func == foo)
   time.sleep(5)
   assert(q.next_job().func == bar)
   assert(q.has_next_job() == False)
+
+  q.add_job(foo, delay=1, name="foo")
+  q.add_job(bar, delay=2, name="bar")
+  time.sleep(1.1)
+  assert(q.remove_job("foo") == True)
+  assert(q.remove_job("foo") == False)
+  assert(q.remove_job("bar") == True)
+  assert(q.has_next_job() == False)
+  assert(q.remove_job("bar") == False)
+
   print "All tests passed."
