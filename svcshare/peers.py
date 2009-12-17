093093f515672fe050cc7a3dbe4ef3a6265791b5
Matt Sparks <ms@quadpoint.org>
2009-12-17 00:17:34 -0600

    Added Peer and PeerNetwork classes and tests.

---
+++ svcshare/peers.py
@@ -0,0 +1,44 @@
+import time
+import exc
+
+
+class Peer(object):
+  def __init__(self, name):
+    self._name = name
+
+  def name(self):
+    return self._name
+
+
+class PeerNetwork(object):
+  """Network of Peers"""
+  def __init__(self):
+    self._peers = {}
+
+  def peerIs(self, peer):
+    if peer.name() in self._peers:
+      # XXX need logging
+      raise exc.NameInUseException
+    self._peers[peer.name()] = peer
+
+  def peerDel(self, name):
+    try:
+      peer = self._peers[name]
+    except KeyError:
+      # XXX need logging
+      raise exc.NameNotFoundException
+    else:
+      del self._peers[name]
+      return peer
+
+  def networkEmpty(self):
+    self._peers = {}
+
+  def peers(self):
+    return len(self._peers.keys())
+
+  def peer(self, name):
+    try:
+      return self._peers[name]
+    except KeyError:
+      return None
---
+++ svcshare/tests/test_peers.py
@@ -0,0 +1,44 @@
+from nose.tools import *
+from svcshare import peers
+from svcshare import exc
+
+
+class Test_Peers:
+  def setup(self):
+    self.network = peers.PeerNetwork()
+
+  def test_emptyNetwork(self):
+    self.network.networkEmpty()
+    assert_equal(self.network.peers(), 0)
+
+  def test_addPeers(self):
+    peer1 = peers.Peer("Bob")
+    peer2 = peers.Peer("Alice")
+    self.network.peerIs(peer1)
+    self.network.peerIs(peer2)
+    assert_equal(self.network.peers(), 2)
+    assert_equal(self.network.peer("Bob"), peer1)
+    assert_equal(self.network.peer("Alice"), peer2)
+
+  def test_removeAllPeers(self):
+    peer = peers.Peer("Bob")
+    self.network.peerIs(peer)
+    assert_equal(self.network.peers(), 1)
+    self.network.networkEmpty()
+    assert_equal(self.network.peers(), 0)
+
+  def test_getUnknownPeer(self):
+    assert_equal(self.network.peer("Fred"), None)
+
+  @raises(exc.NameNotFoundException)
+  def test_removeUnknownPeer(self):
+    peer = self.network.peerDel("Fred")
+
+  def test_removeOnePeer(self):
+    peer1 = peers.Peer("Bob")
+    peer2 = peers.Peer("Alice")
+    self.network.peerIs(peer1)
+    self.network.peerIs(peer2)
+    self.network.peerDel("Bob")
+    assert_equal(self.network.peers(), 1)
+    assert_equal(self.network.peer("Bob"), None)
