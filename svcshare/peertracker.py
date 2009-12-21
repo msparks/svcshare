6b36c170ce2e2ce347ace130b091f6034229c259
Matt Sparks <ms@quadpoint.org>
2009-12-20 22:35:27 -0600

    new PeerTracker using reactor model

--- svcshare/peertracker.py
+++ svcshare/peertracker.py
@@ -1,27 +1,48 @@
-import time
+import logging
+
+from svcshare import exc
+from svcshare import network
+from svcshare import peers
 
 
-class PeerTracker(object):
+class PeerTracker(network.Network.Notifiee):
   def __init__(self):
-    self.clear()
+    network.Network.Notifiee.__init__(self)
+    self._logger = logging.getLogger('PeerTracker')
+    self._peerNetwork = peers.PeerNetwork()
 
-  def clear(self):
-    self._peers = {}
-    self._last_cleared = time.time()
+  def peerNetwork(self):
+    return self._peerNetwork
 
-  def add(self, peername):
-    self._peers[peername] = time.time()
+  def onLeaveEvent(self, event):
+    peerName = event.source().split('!')[0]
 
-  def remove(self, peername):
-    if peername in self._peers:
-      del self._peers[peername]
+    if peerName == self._notifier.nick():
+      self._logger.debug('we left the control channel, flushing peer network')
+      self._peerNetwork.networkEmpty()
+      return
 
-  def rename(self, old_name, new_name):
-    if old_name in self._peers:
-      self._peers[new_name] = self._peers[old_name]
-      self.remove(old_name)
+    try:
+      self._peerNetwork.peerDel(peerName)
+    except exc.NameNotFoundException:
+      self._logger.debug('%s not found in peer network' % peerName)
+    else:
+      self._logger.debug('%s removed from the peer network' % peerName)
 
-  def peers(self):
-    lst = self._peers.keys()
-    lst.sort()
-    return lst
+  def onControlMessage(self, event):
+    peer = peers.Peer(event.source().split('!')[0])
+    args = event.arguments()
+    if len(args) == 1 and args[0] == 'SS_ANNOUNCE':
+      self._onAnnounce(peer, args[1:])
+
+  def _onAnnounce(self, peer, args):
+    self._logger.debug('received announce message from %s' % peer.name())
+    try:
+      self._peerNetwork.peerIs(peer)
+    except exc.NameInUseException:
+      self._logger.critical('peer announcement came from a peer already in '
+                            'the peer network')
+      raise
+    else:
+      self._logger.debug('%s added to the peer network' % peer.name())
+      self._notifier.controlMessageIs(peer.name(), 'SS_ACK')
