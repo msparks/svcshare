afcce80852e9d034edd9198fa7951bfb6011535b
Matt Sparks <ms@quadpoint.org>
2010-11-15 01:56:35 -0800

    Adding LockSet and tests

---
+++ svcshare/lockset.py
@@ -0,0 +1,76 @@
+import logging
+
+
+class LockSetItem(object):
+  def __init__(self, name, locked):
+    self._name = name
+    self._locked = locked
+
+  def __eq__(self, other):
+    return (self._name == other._name and self._locked == other._locked)
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __str__(self):
+    return '(%s, %d)' % (self._name, int(self._locked))
+
+  def __repr__(self):
+    return self.__str__()
+
+  def name(self):
+    return self._name
+
+  def locked(self):
+    return self._locked
+
+
+class LockSet(object):
+  def __init__(self):
+    self._items = {}
+    self._logger = logging.getLogger('LockSet')
+
+  def __eq__(self, other):
+    if other.items() != self.items():
+      return False
+    for key in self._items:
+      if other.item(key) != self.item(key):
+        return False
+    return True
+
+  def __str__(self):
+    return str([self._items[x] for x in sorted(self._items.keys())])
+
+  def __repr__(self):
+    return self.__str__()
+
+  def itemIs(self, item):
+    self._items[item.name()] = item
+
+  def items(self):
+    return len(self._items)
+
+  def item(self, name):
+    try:
+      return self._items[name]
+    except KeyError:
+      return None
+
+  def itemDict(self):
+    return self._items
+
+  def string(self):
+    items = []
+    for key in sorted(self._items.keys()):
+      items.append('%s:%d' % (key, int(self._items[key].locked())))
+    return ' '.join(items)
+
+  def stringIs(self, string):
+    self._items = {}
+    tokens = string.split(' ')
+    for token in tokens:
+      if token.count(':') != 1:
+        continue
+      name, locked = token.split(':')
+      locked = bool(int(locked))
+      self.itemIs(LockSetItem(name, locked))
---
+++ svcshare/tests/test_lockset.py
@@ -0,0 +1,34 @@
+from nose.tools import *
+from svcshare import lockset
+
+
+class Test_LockSet:
+  def setup(self):
+    self._ls = lockset.LockSet()
+    self._item1 = lockset.LockSetItem('l1', False)
+    self._item2 = lockset.LockSetItem('l2', False)
+    self._itemList = [self._item1, self._item2]
+    for item in self._itemList:
+      self._ls.itemIs(item)
+    self._setString = 'l1:0 l2:0'
+
+  def test_insert(self):
+    ls = lockset.LockSet()
+    assert_equals(ls.items(), 0)
+
+    ls.itemIs(self._item1)
+    assert_equals(ls.items(), 1)
+    assert_equals(ls.item(self._item1.name()), self._item1)
+
+    ls.itemIs(self._item2)
+    assert_equals(ls.items(), 2)
+    assert_equals(ls.item(self._item2.name()), self._item2)
+
+  def test_string(self):
+    assert_equals(self._ls.string(), self._setString)
+
+  def test_stringIs(self):
+    ls = lockset.LockSet()
+    ls.stringIs(self._ls.string())
+    assert_equals(ls, self._ls)
+    assert_equals(ls.string(), self._ls.string())
